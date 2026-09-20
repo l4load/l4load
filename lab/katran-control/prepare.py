af0fc5c70ba9c9fa195d74d61164a65efc2f8c05
Viacheslav Guzev <wcbash@icloud.com>
2026-09-20 14:35:03 +0300

    Use C++20 for probe

--- lab/katran-control/prepare.py
+++ lab/katran-control/prepare.py
@@ -8,5 +8,6 @@ shutil.copyfile(probe, upstream / 'l4load_freeze_probe.cpp')
 cmake = upstream / 'CMakeLists.txt'
 cmake.write_text(cmake.read_text() + '''
 add_executable(l4load-freeze-probe l4load_freeze_probe.cpp)
+target_compile_features(l4load-freeze-probe PRIVATE cxx_std_20)
 target_link_libraries(l4load-freeze-probe PRIVATE katranlb Folly::folly)
 ''')
